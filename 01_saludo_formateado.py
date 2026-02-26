7736ad0015f9d5168fb8b95fd992dc9a659e946e
estudiante <estudiante@usfq.edu.ec>
2026-02-26 08:04:22 -0500

    Trabajo 1

---
+++ 01_saludo_formateado.py
@@ -0,0 +1,4 @@
+nombre = str(input("¿Cuál es tu nombre?: "))
+edad = int(input("¿Cuál es tu edad?: "))
+ciudad = str(input("Dame tu ciudad: "))
+print("Hola", nombre, "tienes", edad, "años y vives en", ciudad)
