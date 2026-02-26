434db43c4d9dfb8228378f1fc0ecd7d5415fc3fc
estudiante <estudiante@usfq.edu.ec>
2026-02-26 08:20:42 -0500

    Acabo de crear el archivo :)

---
+++ 02_par_impar.py
@@ -0,0 +1,10 @@
+número =int(input("Dame un nùmero entero: "))
+if número % 2 !=0:
+    print("Es impar")
+else:
+    print("Es par")
+edad =int(input("Dame tu edad: "))
+if edad >= 18:
+    print("Eres mayor de edad")
+else: 
+    print("Eres menor de edad")
